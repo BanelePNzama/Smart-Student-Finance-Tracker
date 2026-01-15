36a95d15b6b6b37fa27a83f7421b7848c63e99dd
Banele Promise Nzama <NzamaBP@dutappfactory.tech>
2026-01-15 02:52:01 +0200

    Edit design

--- Finance-tracker/src/app.js
+++ Finance-tracker/src/app.js
@@ -1,18 +1,18 @@
-const incomeForm = document.getElementById("incomeForm");
-const expenseForm = document.getElementById("expenseForm");
+let incomeForm = document.getElementById("incomeForm");
+let expenseForm = document.getElementById("expenseForm");
 
-const totalIncomeEl = document.getElementById("totalIncome");
-const totalExpensesEl = document.getElementById("totalExpenses");
-const balanceEl = document.getElementById("balance");
-const expenseList = document.getElementById("expenseList");
+let totalIncomeEl = document.getElementById("totalIncome");
+let totalExpensesEl = document.getElementById("totalExpenses");
+let balanceEl = document.getElementById("balance");
+let expenseList = document.getElementById("expenseList");
 
 let incomes = JSON.parse(localStorage.getItem("incomes")) || [];
 let expenses = JSON.parse(localStorage.getItem("expenses")) || [];
 incomeForm.addEventListener("submit", function(e) {
   e.preventDefault();
 
-  const source = document.getElementById("incomeSource").value;
-  const amount = Number(document.getElementById("incomeAmount").value);
+  let source = document.getElementById("incomeSource").value;
+  let amount = Number(document.getElementById("incomeAmount").value);
 
   const income = {
     id: Date.now(),
@@ -28,9 +28,9 @@ incomeForm.addEventListener("submit", function(e) {
 expenseForm.addEventListener("submit", function(e) {
   e.preventDefault();
 
-  const name = document.getElementById("expenseName").value;
-  const category = document.getElementById("expenseCategory").value;
-  const amount = Number(document.getElementById("expenseAmount").value);
+  let name = document.getElementById("expenseName").value;
+  let category = document.getElementById("expenseCategory").value;
+  let amount = Number(document.getElementById("expenseAmount").value);
 
   const expense = {
     id: Date.now(),
@@ -50,9 +50,9 @@ function saveData() {
   localStorage.setItem("expenses", JSON.stringify(expenses));
 }
 function updateDashboard() {
-  const totalIncome = incomes.reduce((sum, item) => sum + item.amount, 0);
-  const totalExpenses = expenses.reduce((sum, item) => sum + item.amount, 0);
-  const balance = totalIncome - totalExpenses;
+  let totalIncome = incomes.reduce((sum, item) => sum + item.amount, 0);
+  let totalExpenses = expenses.reduce((sum, item) => sum + item.amount, 0);
+  let balance = totalIncome - totalExpenses;
 
   totalIncomeEl.textContent = "R" + totalIncome;
   totalExpensesEl.textContent = "R" + totalExpenses;
@@ -62,7 +62,7 @@ function renderExpenses() {
   expenseList.innerHTML = "";
 
   expenses.forEach(exp => {
-    const li = document.createElement("li");
+    let li = document.createElement("li");
     li.className = "list-group-item d-flex justify-content-between";
 
     li.innerHTML = `
